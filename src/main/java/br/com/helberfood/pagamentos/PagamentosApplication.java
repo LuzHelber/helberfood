ecbe2d6e089f2a4e4602e552ebe0f85ca83389f8
Helber Luz <helbersap@gmail.com>
2023-06-15 18:01:59 -0300

    implementacao do Ms pedidos

--- src/main/java/br/com/helberfood/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/helberfood/pagamentos/PagamentosApplication.java
@@ -3,7 +3,9 @@ package br.com.helberfood.pagamentos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
